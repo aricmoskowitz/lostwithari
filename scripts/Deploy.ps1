7c53bfd6226322e8e45885bdc34a6620a5a8dd24
aricmoskowitz <31522973+aricmoskowitz@users.noreply.github.com>
2019-08-04 23:27:15 -0700

    More changes for powershell. still broke

--- scripts/Deploy.ps1
+++ scripts/Deploy.ps1
@@ -17,13 +17,88 @@ $artifactDropPath = Join-Path $env:TEMP "lostwithari-out"
 Write-Host ("Staging artifacts in $($artifactDropPath)")
 
 # TODO: Cleanup artifact drop before proceeding
+Remove-Item -Path $artifactDropPath -Recurse -Force
 
 Copy-Item -Path "$($PSScriptRoot)\..\" -Destination $artifactDropPath -Recurse -Force
 # TODO: Insert logic here to only copy files of interest. You should obviously leave out scripts and credential configs.
 
 # TODO: Overwrite a javascript variable with a prod API key.
 
-# start $artifactDropPath
 
 
+function Write-ItemToServer (
+    $FolderName = "/",
+    $Item,
+    $webclient,
+    $IsFolder = $false
+)
+{
+    $FolderName = $FolderName.Replace("\", "/")
+    Write-Host "Uploading to folder $($FolderName)"
+    Write-Host "Uploading $($Item.Name)" 
+
+    $ftpEndpoint = "$($ftpEndpoint)$($FolderName)"
+    Write-Host "FTP ENDPOINT: $($ftpEndpoint)"
+    Write-Host "Full ENDPOINT: $($ftpEndpoint+"/"+$Item.Name)"
+    $uri = New-Object System.Uri($ftpEndpoint+"/"+$Item.Name) 
+
+    if ($IsFolder){
+        $webclient.UploadFile($uri) 
+
+    }
+    else {
+    $webclient.UploadFile($uri, $item.FullName) 
+
+    }
+}
+
+
+function Write-AllItemsToServer (
+    $artifactDropPath,
+    $folder = ""
+)
+{
+    Write-Host "In write-allitemstoserver"
+    Write-Host "Working with artifactdrop: $($artifactDropPath)"
+    Write-Host "Working with folder: $($folder)"
+
+    if ($folder)
+    {
+        $workingPath = Join-Path $artifactDropPath $folder
+    }
+    else 
+    {
+        $workingPath = $artifactDropPath
+    }
+
+    foreach($item in (Get-ChildItem $workingPath -File)){ 
+        Write-ItemToServer -Item $item -webclient $webclient -FolderName $folder
+    } 
+
+    foreach($item in (Get-ChildItem $workingPath -Directory)){ 
+
+        if (!($item.Name[0] -eq "."))
+        {
+            Write-ItemToServer -Item $item -webclient $webclient -FolderName $folder -IsFolder $true
+            Write-AllItemsToServer -artifactDropPath $artifactDropPath -folder "$folder\$($item.Name)"
+        }
+        else {
+            Write-Host "Skipping directory $($item.Name)"
+        }
+    }
+}
+
+
+$webclient = New-Object System.Net.WebClient 
+ 
+$webclient.Credentials = New-Object System.Net.NetworkCredential($userName,$password)  
+ 
+Write-AllItemsToServer -artifactDropPath $artifactDropPath
+
+# foreach($item in (Get-ChildItem $artifactDropPath -File)){ 
+#     Write-ItemToServer -Item $item -webclient $webclient
+#     # "Uploading $item..." 
+#     # $uri = New-Object System.Uri($ftpEndpoint+$item.Name) 
+#     # $webclient.UploadFile($uri, $item.FullName) 
+#  } 
 
