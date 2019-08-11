ec8e3d901a19fe3001c83ed7ca0f77d2dbe9dec6
aricmoskowitz <31522973+aricmoskowitz@users.noreply.github.com>
2019-08-10 23:39:29 -0400

    Back to blue colors & added variables to Index.js

--- js/index.js
+++ js/index.js
@@ -1,36 +1,42 @@
 var earth;
 var homeCoords = [40.888193, -73.436393];
 var currentCoords = [-33.8696, 151.20695];
+var currentLocation = 'Sydney, AUS';
+var markerColor = 'FCCA46';
+
 
 function initialize() {
-  earth = new WE.map('map',{
-    zooming: false
-  });
-  earth.setView([40.888193, -73.436393], 4);
-  WE.tileLayer('https://webglearth.github.io/webglearth2-offline/{z}/{x}/{y}.jpg', {
-    tileSize: 256,
-    bounds: [[-85, -180], [85, 180]],
-    minZoom: 0,
-    maxZoom: 20,
-    tms: true,
-  }).addTo(earth);
+    earth = new WE.map('map', {
+        zooming: false
+    });
+    earth.setView([40.888193, -73.436393], 4);
+    WE.tileLayer('https://webglearth.github.io/webglearth2-offline/{z}/{x}/{y}.jpg', {
+        tileSize: 256,
+        bounds: [
+            [-85, -180],
+            [85, 180]
+        ],
+        minZoom: 0,
+        maxZoom: 20,
+        tms: true,
+    }).addTo(earth);
 
-  var markerHome = WE.marker(homeCoords, 'http://chart.apis.google.com/chart?chst=d_map_spin&chld=0.7|0|FCCA46|13|b|H').addTo(earth)
-  markerHome.bindPopup('<b>Home</b>', 100);
+    var markerHome = WE.marker(homeCoords, 'http://chart.apis.google.com/chart?chst=d_map_spin&chld=0.7|0|' + markerColor + '|13|b|H').addTo(earth)
+    markerHome.bindPopup('<b>Home</b>', 100);
 
 
-  var markerCurrent = WE.marker(currentCoords,'http://chart.apis.google.com/chart?chst=d_map_spin&chld=0.7|0|FCCA46|13|b|').addTo(earth)
-  markerCurrent.bindPopup('<b>Sydney, AUS</b>', 100);
-  markerCurrent.openPopup();
+    var markerCurrent = WE.marker(currentCoords, 'http://chart.apis.google.com/chart?chst=d_map_spin&chld=0.7|0|' + markerColor + '|13|b|').addTo(earth)
+    markerCurrent.bindPopup('<b>' + currentLocation + '</b>', 100);
+    markerCurrent.openPopup();
 
 
 }
 
 function toCurrent() {
-  setTimeout(function() {earth.panTo(currentCoords,{heading: 90, tilt: 25, duration: 1})}, 3500);
+    setTimeout(function() { earth.panTo(currentCoords, { heading: 90, tilt: 25, duration: 1 }) }, 1500);
 }
 
 function startUp() {
-  initialize();
-  toCurrent();
+    initialize();
+    toCurrent();
 }
