685038b0f11e87056061086f467672106dea5601
aricmoskowitz <31522973+aricmoskowitz@users.noreply.github.com>
2019-12-15 00:31:05 +0900

    Changed Current Coords

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 var earth;
 var homeCoords = [40.888193, -73.436393];
-var currentCoords = [33.5633, 130.7116];
-var currentLocation = 'Kama, Japan';
+var currentCoords = [37.5665, 126.9780];
+var currentLocation = 'Seoul, South Korea';
 var markerColor = 'FCCA46';
 
 
